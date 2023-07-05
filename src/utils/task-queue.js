22f973068b3c8892e03a6618b97786a56d5afb60
Larry Hudson <10627494+larryhudson@users.noreply.github.com>
2023-07-06 07:49:16 +1000

    WIP

--- src/utils/task-queue.js
+++
@@ -1,8 +0,0 @@
-import {Queue} from "bullmq"
-
-export async function addToQueue({queueName, jobName, jobData}) {
-
-  const taskQueue = new Queue(queueName);
-
-  return taskQueue.add(jobName, jobData);  
-}
