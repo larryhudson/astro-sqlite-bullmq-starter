398524e43d808aa321d06f99fb0ddcfc01600ecd
Larry Hudson <10627494+larryhudson@users.noreply.github.com>
2023-10-17 07:19:59 +1100

    feat: add htmx

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,24 +1,27 @@
-import Database from 'better-sqlite3';
+import Database from "better-sqlite3";
 
 // Connect to the SQLite database
-const db = new Database('sqlite.db', {verbose: console.log});
+const db = new Database("sqlite.db", { verbose: console.log });
 db.pragma("journal_mode = WAL");
 
 export function createRecord(table, data) {
   const keys = Object.keys(data);
   const values = Object.values(data);
-  const placeholders = values.map(() => '?').join(', ');
-  const statement = db.prepare(`INSERT INTO ${table} (${keys.join(', ')}) VALUES (${placeholders})`);
+  const placeholders = values.map(() => "?").join(", ");
+  const statement = db.prepare(
+    `INSERT INTO ${table} (${keys.join(", ")}) VALUES (${placeholders})`,
+  );
   const result = statement.run(values);
   return result.lastInsertRowid;
 }
 
 function parseId(id) {
+  console.log({ id });
 
   const parsedId = parseInt(id);
 
   if (Number.isNaN(parsedId)) {
-    throw new Error('Invalid ID. Please provide a valid integer ID.');
+    throw new Error("Invalid ID. Please provide a valid integer ID.");
   }
 
   return parsedId;
@@ -37,24 +40,26 @@ export function updateRecord(table, id, data) {
 
   const keys = Object.keys(data);
   const values = Object.values(data);
-  const placeholders = keys.map(key => `${key} = ?`).join(', ');
-  const statement = db.prepare(`UPDATE ${table} SET ${placeholders} WHERE id = ?`);
+  const placeholders = keys.map((key) => `${key} = ?`).join(", ");
+  const statement = db.prepare(
+    `UPDATE ${table} SET ${placeholders} WHERE id = ?`,
+  );
   const result = statement.run([...values, parsedId]);
   return result.changes > 0;
 }
 
 export function executeQuery(table, condition) {
-  let whereClause = '';
+  let whereClause = "";
   const values = [];
 
   if (condition) {
     const keys = Object.keys(condition);
-    const conditions = keys.map(key => {
+    const conditions = keys.map((key) => {
       values.push(condition[key]);
       return `${key} = ?`;
     });
 
-    whereClause = `WHERE ${conditions.join(' AND ')}`;
+    whereClause = `WHERE ${conditions.join(" AND ")}`;
   }
 
   const query = `SELECT * FROM ${table} ${whereClause}`;
@@ -69,4 +74,3 @@ export function deleteRecordById(table, id) {
   const result = statement.run(parsedId);
   return result.changes > 0;
 }
-
